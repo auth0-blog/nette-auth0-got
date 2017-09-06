a9d2d1197117b5256ddd059d7acb7254aa784573
unicodeveloper <prosperotemuyiwa@gmail.com>
2017-09-06 12:58:20 +0100

    Settle auth parts

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -17,10 +17,10 @@ class RouterFactory
 	public static function createRouter()
 	{
 		$router = new RouteList;
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
 		$router[] = new Route('login', 'Authentication:login');
 		$router[] = new Route('logout', 'Authentication:logout');
 		$router[] = new Route('callback', 'Authentication:callback');
+		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
 		return $router;
 	}
 }
